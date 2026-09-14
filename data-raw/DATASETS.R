69d6578999db1d57a294b84537bcd317d732ec37
Kostas Vasilopoulos <k.vasilopoulo@gmail.com>
2026-09-14 15:18:04 +0300

    Add ten maintained series: Wu-Xia, Fernald TFP, EPU, IGREA, JLN, JK shocks, Mertens-Ravn, GPR, Shiller, Ramey 2011
    - wx2016: shadow fed funds rate (Atlanta Fed, 1960-2022)
    - f2014: utilization-adjusted TFP (SF Fed, 1947-2026)
    - bbd2016: US economic policy uncertainty (1900-2026)
    - k2009: Kilian global real economic activity index (Dallas Fed)
    - jln2015: macro/financial/real uncertainty at h=1,3,12
    - jk2020: monetary policy vs central bank information shocks, monthly
    - mr2013: narrative personal/corporate tax shocks and SVAR data
    - ci2022: geopolitical risk, recent + historical + 44 countries
    - s2015: Shiller S&P/dividends/earnings/CAPE from 1871
    - r2011: Ramey defense news and fiscal aggregates 1939-2008

--- data-raw/DATASETS.R
+++ data-raw/DATASETS.R
@@ -141,3 +141,85 @@ gz2012 <- read_csv("data-raw/gz2012/ebp.csv", show_col_types = FALSE) %>%
   mutate(date = as.Date(date, "%m/%d/%Y"))
 
 usethis::use_data(gz2012, overwrite = TRUE)
+
+
+# Batch 2: maintained series from author / Fed websites ###################
+# Raw files and download URLs are listed in data-raw/TODO.md.
+
+month_date <- function(year, month) as.Date(sprintf("%d-%02d-01", as.integer(year), as.integer(month)))
+
+# Wu and Xia (2016) shadow rate (Atlanta Fed, stopped 2022-02) -----------
+wx2016 <- readxl::read_excel("data-raw/wx2016/WuXiaShadowRate.xlsx", sheet = "Data",
+                             range = readxl::cell_cols("A:C"), col_names = c("date", "ff", "shadow"), skip = 1) %>%
+  filter(!is.na(date)) %>%
+  mutate(date = as.Date(date), across(-date, as.numeric))
+usethis::use_data(wx2016, overwrite = TRUE)
+
+# Fernald (2014) utilization-adjusted TFP (SF Fed, quarterly) -----------
+f2014 <- readxl::read_excel("data-raw/f2014/quarterly_tfp.xlsx", sheet = "quarterly", skip = 1) %>%
+  filter(grepl("^[0-9]{4}:Q[1-4]$", date)) %>%
+  mutate(date = lubridate::yq(date), across(-date, as.numeric))
+usethis::use_data(f2014, overwrite = TRUE)
+
+# Baker, Bloom and Davis (2016) US economic policy uncertainty ------------
+epu_news <- readxl::read_excel("data-raw/bbd2016/US_Policy_Uncertainty_Data.xlsx", sheet = "Main News Index") %>%
+  filter(!is.na(Month)) %>% rename(epu_news = 3)
+epu_3 <- readxl::read_excel("data-raw/bbd2016/US_Policy_Uncertainty_Data.xlsx", sheet = "Legacy Three Component EPU") %>%
+  filter(!is.na(Month)) %>%
+  set_names("Year", "Month", "epu_3comp", "epu_news_legacy", "epu_govt_disagreement", "epu_cpi_disagreement", "epu_tax_expiration")
+bbd2016 <- full_join(epu_news, epu_3, by = c("Year", "Month")) %>%
+  mutate(date = month_date(Year, Month), Year = NULL, Month = NULL) %>%
+  select(date, everything()) %>%
+  arrange(date)
+usethis::use_data(bbd2016, overwrite = TRUE)
+
+# Kilian (2009) global real economic activity index (Dallas Fed) ----------
+k2009 <- readxl::read_excel("data-raw/k2009/igrea.xlsx", col_names = c("date", "igrea"), skip = 1) %>%
+  mutate(date = lubridate::floor_date(as.Date(date), "month"))
+usethis::use_data(k2009, overwrite = TRUE)
+
+# Jurado, Ludvigson and Ng (2015) uncertainty indexes ---------------------
+jln_read <- function(kind) {
+  readxl::read_excel(sprintf("data-raw/jln2015/%sUncertaintyToCirculate.xlsx", kind), sheet = 1) %>%
+    set_names("date", paste0(tolower(kind), "_h", c(1, 3, 12))) %>%
+    mutate(date = as.Date(date))
+}
+jln2015 <- jln_read("Macro") %>% full_join(jln_read("Financial"), by = "date") %>% full_join(jln_read("Real"), by = "date")
+usethis::use_data(jln2015, overwrite = TRUE)
+
+# Jarocinski and Karadi (2020) monetary policy and information shocks -----
+jk2020 <- read_csv("data-raw/jk2020/shocks_fed_gss_pr_median_m.csv", show_col_types = FALSE) %>%
+  mutate(date = month_date(year, month), year = NULL, month = NULL) %>%
+  rename_with(tolower) %>%
+  select(date, everything())
+usethis::use_data(jk2020, overwrite = TRUE)
+
+# Mertens and Ravn (2013) narrative tax shocks ----------------------------
+mr2013 <- readxl::read_excel("data-raw/mr2013/MR_AER_DATASET.xlsx", sheet = "Quarterly") %>%
+  rename(date = DATES) %>%
+  mutate(date = frac_to_date(date, 4))
+usethis::use_data(mr2013, overwrite = TRUE)
+
+# Caldara and Iacoviello (2022) geopolitical risk -------------------------
+ci2022 <- readxl::read_excel("data-raw/ci2022/data_gpr_export.xls", guess_max = 2000) %>%
+  select(month, GPR:N3H, starts_with("SHAREH_CAT"), starts_with("GPRC_")) %>%
+  rename(date = month) %>%
+  mutate(date = as.Date(date))
+usethis::use_data(ci2022, overwrite = TRUE)
+
+# Shiller (2015) Irrational Exuberance online data ------------------------
+s2015 <- readxl::read_excel("data-raw/s2015/ie_data.xls", sheet = "Data", skip = 8, col_names = FALSE,
+                            col_types = "numeric") %>%
+  select(1:13, 15, 17:22) %>%
+  set_names("date", "price", "dividend", "earnings", "cpi", "date_frac", "gs10", "real_price", "real_dividend",
+            "real_tr_price", "real_earnings", "real_tr_earnings", "cape", "tr_cape", "ecy",
+            "bond_return", "real_bond_return", "stock_real_return_10y", "bond_real_return_10y", "excess_return_10y") %>%
+  filter(!is.na(date)) %>%
+  mutate(date = month_date(floor(date), round((date - floor(date)) * 100)), date_frac = NULL)
+usethis::use_data(s2015, overwrite = TRUE)
+
+# Ramey (2011) government spending shocks ---------------------------------
+r2011 <- readxl::read_excel("data-raw/r2011/Ramey_Govt_Public_Data.xls", sheet = "Data for 1939-2008", skip = 4) %>%
+  rename(date = quarter) %>%
+  mutate(date = frac_to_date(date, 4))
+usethis::use_data(r2011, overwrite = TRUE)
